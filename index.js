859ed08c11135905b26703a676a4dcb3b4529e14
fran-cielle <nasc.francielle@gmail.com>
2020-01-14 15:25:06 -0300

    conectando com o banco de dados

--- index.js
+++ index.js
@@ -1,6 +1,11 @@
 const express = require('express');
+const mongoose = require('mongoose');
 
 const app = express();
+mongoose.connect('mongodb+srv://francielle_adm:eusoudev@cluster0-1ha6j.mongodb.net/estudando?retryWrites=true&w=majority', {
+    useNewUrlParser: true,
+    useUnifiedTopology: true 
+})
 
 app.use(express.json()); 
 
