6523b73da5a4edb0070a272f37205451654d06b9
fran-cielle <nasc.francielle@gmail.com>
2020-01-14 18:23:45 -0300

    inserindo um novo dev e coletando informacoes da api do github

---
+++ src/controller/devController.js
@@ -0,0 +1,30 @@
+const { connect } = require('../models/Repository');
+const devModel = require('../models/devSchema');
+const axios = require('axios');
+
+connect()
+
+
+const addDev = async (request, response ) =>{
+    const {github_username, techs } = request.body;
+    const apiResponse = await axios.get(`https://api.github.com/users/${github_username}`);
+
+    const { name, avatar_url, bio } = apiResponse.data;
+
+    const techsArray = techs.split(',').map(tech => tech.trim());
+
+    const dev = await devModel.create({
+        github_username:github_username,
+        name,
+        avatar_url,
+        bio,
+        techs: techsArray
+    })
+
+    return response.json(dev).status(201)
+}
+
+
+module.exports = {
+    addDev
+}
