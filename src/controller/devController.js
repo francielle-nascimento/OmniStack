da7cc638d64f53d90a6e49d5d9c545a3ffd0d380
fran-cielle <nasc.francielle@gmail.com>
2020-01-14 18:46:36 -0300

    inserindo controller para coletar a localizacao

--- src/controller/devController.js
+++ src/controller/devController.js
@@ -6,19 +6,26 @@ connect()
 
 
 const addDev = async (request, response ) =>{
-    const {github_username, techs } = request.body;
+    const {github_username, techs, latitude, longitude } = request.body;
     const apiResponse = await axios.get(`https://api.github.com/users/${github_username}`);
 
     const { name, avatar_url, bio } = apiResponse.data;
 
     const techsArray = techs.split(',').map(tech => tech.trim());
 
+    const location = {
+        type: 'Point',
+        coordinates:[longitude, latitude]
+    }
+
+
     const dev = await devModel.create({
         github_username:github_username,
         name,
         avatar_url,
         bio,
-        techs: techsArray
+        techs: techsArray,
+        location
     })
 
     return response.json(dev).status(201)
